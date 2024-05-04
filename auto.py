25695f106e70214b55affa3d8622810195e40b98
kira-DX <kurakeyna@hotmail.com>
2024-05-04 18:57:20 +0900

    yuruchi

--- auto.py
+++ auto.py
@@ -1,7 +1,7 @@
 import add_json
 import generate
 
-category = 'モノマネ'
-button_name = 'ドラムロール'
+category = '名言'
+button_name = '昨日も綺麗だったよ、ゆるちは。'
 add_json.update_json_data(category, button_name)
 generate.generate()
