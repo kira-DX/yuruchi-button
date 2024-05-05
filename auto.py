e33d0ab2e5d07bb9d7e76358d51bb76d65f11820
kira-DX <kurakeyna@hotmail.com>
2024-05-05 13:47:52 +0900

    yuruchi

--- auto.py
+++ auto.py
@@ -1,7 +1,7 @@
 import add_json
 import generate
 
-category = 'その他'
-button_name = 'なんで？（イケボ）'
+category = '名言'
+button_name = '夜にやりがちゆるち'
 add_json.update_json_data(category, button_name)
 generate.generate()
