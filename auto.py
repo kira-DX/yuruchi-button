b316bc87ce8882f3611860bf62a70c520f9cdb78
kira-DX <kurakeyna@hotmail.com>
2024-05-06 03:54:56 +0900

    yuruchi

--- auto.py
+++ auto.py
@@ -1,7 +1,7 @@
 import add_json
 import generate
 
-category = 'ＩＱ３'
-button_name = '蛙が先か鶏が先か'
+category = 'モノマネ'
+button_name = 'マリオにいじめられるキノピオ'
 add_json.update_json_data(category, button_name)
 generate.generate()
