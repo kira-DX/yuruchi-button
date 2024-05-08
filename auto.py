7f145b55c06f53a1e200885ce6947403422a6c09
kira-DX <kurakeyna@hotmail.com>
2024-05-08 23:35:44 +0900

    add

--- auto.py
+++ auto.py
@@ -1,7 +1,7 @@
 import add_json
 import generate
 
-category = '奇声'
-button_name = 'パパ、パパ、パァ〜'
+category = ''
+button_name = ''
 add_json.update_json_data(category, button_name)
 generate.generate()
