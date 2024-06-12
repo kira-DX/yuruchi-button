096b88ea5dd8af5c1e0e3215ddd4455631339aac
kira-DX <kurakeyna@hotmail.com>
2024-06-12 19:07:37 +0900

    add

--- auto.py
+++ auto.py
@@ -1,7 +1,7 @@
 import add_json
 import generate
 
-category = ''
-button_name = ''
+category = 'キレ芸'
+button_name = 'やってられっかこんな配信！おつちゅっちゅるゥ'
 add_json.update_json_data(category, button_name)
 generate.generate()
