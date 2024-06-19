511c99fdd435f744f18ee8f4aff8c97dc7afdb81
kira-DX <kurakeyna@hotmail.com>
2024-06-19 22:33:05 +0900

    add

--- auto.py
+++ auto.py
@@ -1,7 +1,7 @@
 import add_json
 import generate
 
-category = ''
-button_name = ''
+category = '名言'
+button_name = 'すいません…勝てそうです'
 add_json.update_json_data(category, button_name)
 generate.generate()
