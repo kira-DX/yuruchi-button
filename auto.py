c0f9dbf99f4d5977691809aa92d9db93aad17ca0
kira-DX <kurakeyna@hotmail.com>
2024-10-09 00:54:01 +0900

    add

--- auto.py
+++ auto.py
@@ -1,6 +1,6 @@
 import develope
 
-develope.add_audio('モノマネ','ブウ')
+develope.add_audio('キレ芸','誰゛た゛こ゛ん゛な゛と゛こ゛ろ゛に゛ハ゛ナ゛ナ゛置゛い゛た゛や゛つ゛は゛ー゛')
 # develope.del_audio('モノマネ','ブウ')
 # develope.change_audio('その他','test','test2.mp3')
 # init_lang_file()
