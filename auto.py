43f5f01fd1ff29954be272dbf575c23f6cd10a9f
kira-DX <kurakeyna@hotmail.com>
2025-10-07 00:03:23 +0900

    配信用ボタン追加

--- auto.py
+++ auto.py
@@ -1,6 +1,7 @@
 import develope
 
-develope.add_audio('配信用','クソ.mp3')
+develope.add_audio('配信用','うんち！アンチ！.mp3')
+develope.add_audio('配信用','さてはアンチだなオメー.mp3')
 #develope.del_audio('モノマネ','目覚ましボイス')
 #develope.change_audio('配信用','（よろしくお願いします）','クソ.mp3')
 #init_lang_file()
