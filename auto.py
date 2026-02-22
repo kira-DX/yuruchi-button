f5cfe2e0efa8bb68e4ee627f18afaacb2d7de706
kira-DX <kurakeyna@hotmail.com>
2026-02-23 01:46:49 +0900

    add：おすじょぉ～

--- auto.py
+++ auto.py
@@ -1,7 +1,6 @@
 import develope
 
-develope.add_audio('配信用','うんち！アンチ！.mp3')
-develope.add_audio('配信用','さてはアンチだなオメー.mp3')
+develope.add_audio('配信用','おすじょぉ～.mp3')
 #develope.del_audio('モノマネ','目覚ましボイス')
 #develope.change_audio('配信用','（よろしくお願いします）','クソ.mp3')
 #init_lang_file()
