42e42d0395c794471f308a3d2e6a2ff0681f5871
kira-DX <kurakeyna@hotmail.com>
2026-03-02 20:37:29 +0900

    ありがとう追加

--- auto.py
+++ auto.py
@@ -1,6 +1,11 @@
 import develope
 
-develope.add_audio('配信用','おすじょぉ～.mp3')
+#develope.add_audio('配信用','ありがとう.mp3')
+#develope.add_audio('配信用','ありがとう（あざと）.mp3')
+#develope.add_audio('配信用','ありがとう（かわいいver）.mp3')
+#develope.add_audio('配信用','ありがとう（恥）.mp3')
+#evelope.add_audio('配信用','さっき感謝したでしょーが！.mp3')
 #develope.del_audio('モノマネ','目覚ましボイス')
-#develope.change_audio('配信用','（よろしくお願いします）','クソ.mp3')
+develope.change_audio('配信用','ありがとう（恥）','ありがとう（恥）.mp3')
+develope.change_audio('配信用','さっき感謝したでしょーが！','さっき感謝したでしょーが！.mp3')
 #init_lang_file()
